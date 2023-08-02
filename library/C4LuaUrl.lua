cb19795a067da1c08bc259c99c120ced244a86cb
annex4 <82889064+annex4@users.noreply.github.com>
2023-08-02 15:05:39 -0400

    Made all C4LuaUrlOptions optional.

--- library/C4LuaUrl.lua
+++ library/C4LuaUrl.lua
@@ -22,22 +22,22 @@
 ---@field TicketId fun(): number
 
 ---@class C4LuaUrlOptions
----@field fail_on_error boolean
----@field timeout number In seconds. Defaults to 300 or whatever value was set through C4:urlSetTimeout(). This is how many seconds, total, the entire transfer is allowed to take.
----@field connect_timeout number In seconds. Defaults to 5. This is how many seconds it is allowed to take to establish the connection to the host.
----@field ssl_verify_host boolean Defaults to true. Verify the host against the system's CA bundle when using the https protocol.
----@field ssl_verify_peer boolean Defaults to true. Verify the peer against the system's CA bundle when using the https protocol.
----@field ssl_cabundle string A filename relative to the .c4z that specifies a CA bundle to use instead of the system's. It is not recommended to use this option except for very special use cases because CA bundles require regular updates.
----@field ssl_cert string A filename relative to the .c4z that specifies a SSL client certificate to use for authentication to the host.
----@field ssl_cert_type string # Defaults to 'PEM'. The format of the SSL certificate that the file specified by the 'ssl_cert' option is in. Valid values are: 'PEM' (default), 'DER', 'P12'.
+---@field fail_on_error? boolean
+---@field timeout number? In seconds. Defaults to 300 or whatever value was set through C4:urlSetTimeout(). This is how many seconds, total, the entire transfer is allowed to take.
+---@field connect_timeout? number In seconds. Defaults to 5. This is how many seconds it is allowed to take to establish the connection to the host.
+---@field ssl_verify_host? boolean Defaults to true. Verify the host against the system's CA bundle when using the https protocol.
+---@field ssl_verify_peer? boolean Defaults to true. Verify the peer against the system's CA bundle when using the https protocol.
+---@field ssl_cabundle? string A filename relative to the .c4z that specifies a CA bundle to use instead of the system's. It is not recommended to use this option except for very special use cases because CA bundles require regular updates.
+---@field ssl_cert? string A filename relative to the .c4z that specifies a SSL client certificate to use for authentication to the host.
+---@field ssl_cert_type? string # Defaults to 'PEM'. The format of the SSL certificate that the file specified by the 'ssl_cert' option is in. Valid values are: 'PEM' (default), 'DER', 'P12'.
 ---| "PEM"
 ---| "DER"
 ---| "P12"
----@field ssl_key string A filename relative to the .c4z that specifies the private key for the client certificate specified by the "ssl_cert" option.
----@field ssl_passwd string If the private key specified by the "ssl_key" option is encrypted, this option specifies the password.
----@field ssl_cacerts table A table of filenames relative to the .c4z that specify additional certificates to be added to the CA bundle used to verify the host and/or peer. This allows e.g. extending the CA bundle to be able to verify against self-signed or company-signed certificates.
----@field response_headers_duplicates boolean If set to true and duplicate response headers are received, the tHeader table will have an array of string values for this header. If set to false (default) and duplicate response headers are received, only the last header received will be saved to the tHeader table. This option defaults to false.
----@field cookies_enable boolean If set to true, enables the use of cookies.  If the cookies_use_jar option is set to true, this causes cookies to be stored in the driver's cookie jar. If set to false (default) and a Set-Cookie header is received, the cookie will neither be stored in the driver's cookie jar, nor will it be used with any redirect request that may automatically be performed. This option defaults to false.
----@field cookies_use_jar boolean If set to true (default), uses cookies in the driver's cookie jar. If set to false, this transfer does not use any cookies that may be stored in the driver's cookie jar. This option defaults to true.
----@field cookies_save_to_jar boolean If set to true (default), saves any new cookies to the driver's cookie jar. This may also delete expired cookies from the cookie jar. If set to false, any cookies received from this transfer will not be saved to the jar. This option defaults to true.
----@field cookies_clear_session boolean If set to true, clears any "session" cookies (cookies with no expiration time) from this transfer prior to making the request.  This does not remove them from the driver's cookie jar, but if the cookies_save_to_jar"option is set to true, it will remove them from the cookie jar once the transfer completes. If set to false (default), session cookies are used for this transfer. This option defaults to false.
+---@field ssl_key? string A filename relative to the .c4z that specifies the private key for the client certificate specified by the "ssl_cert" option.
+---@field ssl_passwd? string If the private key specified by the "ssl_key" option is encrypted, this option specifies the password.
+---@field ssl_cacerts? table A table of filenames relative to the .c4z that specify additional certificates to be added to the CA bundle used to verify the host and/or peer. This allows e.g. extending the CA bundle to be able to verify against self-signed or company-signed certificates.
+---@field response_headers_duplicates? boolean If set to true and duplicate response headers are received, the tHeader table will have an array of string values for this header. If set to false (default) and duplicate response headers are received, only the last header received will be saved to the tHeader table. This option defaults to false.
+---@field cookies_enable? boolean If set to true, enables the use of cookies.  If the cookies_use_jar option is set to true, this causes cookies to be stored in the driver's cookie jar. If set to false (default) and a Set-Cookie header is received, the cookie will neither be stored in the driver's cookie jar, nor will it be used with any redirect request that may automatically be performed. This option defaults to false.
+---@field cookies_use_jar? boolean If set to true (default), uses cookies in the driver's cookie jar. If set to false, this transfer does not use any cookies that may be stored in the driver's cookie jar. This option defaults to true.
+---@field cookies_save_to_jar? boolean If set to true (default), saves any new cookies to the driver's cookie jar. This may also delete expired cookies from the cookie jar. If set to false, any cookies received from this transfer will not be saved to the jar. This option defaults to true.
+---@field cookies_clear_session? boolean If set to true, clears any "session" cookies (cookies with no expiration time) from this transfer prior to making the request.  This does not remove them from the driver's cookie jar, but if the cookies_save_to_jar"option is set to true, it will remove them from the cookie jar once the transfer completes. If set to false (default), session cookies are used for this transfer. This option defaults to false.
